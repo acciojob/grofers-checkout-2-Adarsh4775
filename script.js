322f6c872acbf14c87181c935333c49ef594296a
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-08 17:10:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+let priceElement = document.getElementById("data-ns-prices");
+let totalPrice = 0;
+
+priceElement.forEach((priceElement)=>
+	let price = parseInt(priceElement.textContent);
+			totalPrice += price;		 
+	);
+
+let newRow = document.createElement("tr");
+let newCell = document.createElement("td");
+
+newCell.textContent= `Total: $${totalPrice.toFixed(2)}`;
+
+newRow.appendChild(newCell);
+let table = document.getElementByTagname("table");
+table.appendChild(newRow);
+
+caluculateTotalPrice();
