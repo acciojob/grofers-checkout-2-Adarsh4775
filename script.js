fb7c7427b6f23a5f1be6cb7c7835b791f3306863
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-08 17:43:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
-// Get all elements with data-ns-test="prices"
-const priceElements = document.querySelectorAll('[data-ns-test="prices"]');
-
-// Function to calculate and update the total price
-function updateTotal() {
-    let totalPrice = 0;
-    
-    // Loop through the price elements and sum their values
-    priceElements.forEach((element) => {
-        totalPrice += parseFloat(element.textContent);
+describe("Price Calculation", () => {
+    before(() => {
+        // Visit the HTML page with your table
+        cy.visit("index.html"); // Replace with the actual URL of your page
     });
-    
-    // Get the grand total element
-    const grandTotalElement = document.querySelector('[data-ns-test="grandTotal"]');
-    
-    // Update the grand total element with the calculated total price
-    grandTotalElement.textContent = totalPrice.toFixed(2);
-}
 
-// Call the updateTotal function to calculate and display the initial total
-updateTotal();
+    it("should check the number of prices", () => {
+        // Ensure that there are at least three price elements
+        cy.get('[data-ns-test=price]').should('have.length.at.least', 3);
+    });
+
+    it("should find the grand total element", () => {
+        // Ensure that the grand total element exists
+        cy.get('[data-ns-test=grandTotal]').should('exist');
+    });
+
+    it("should calculate the total correctly", () => {
+        // Calculate the sum of prices
+        cy.get('[data-ns-test=price]').then((priceElements) => {
+            let sum = 0;
+            priceElements.each((index, element) => {
+                sum += parseFloat(element.textContent);
+            });
+
+            // Get the text content of the "grandTotal" element
+            cy.get('[data-ns-test=grandTotal]').then((grandTotalElement) => {
+                let sum_ = grandTotalElement.text();
+                expect(parseFloat(sum_)).to.equal(sum); // Compare the calculated sum with the displayed sum
+            });
+        });
+    });
+});
