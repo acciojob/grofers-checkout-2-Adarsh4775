0d0e48e8a9f9541e1d5da90543a8115e4f5b9446
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-08 17:33:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-//your code here
-let priceElement = document.querySelectorAll(".data-ns-prices");
-let totalPrice = 0;
+// Get all elements with data-ns-test="prices"
+const priceElements = document.querySelectorAll('[data-ns-test="prices"]');
 
-priceElement.forEach((priceElement)=>{
-	let price = parseInt(priceElement.textContent);
-			totalPrice += price;		 
-});
+// Function to calculate and update the total price
+function updateTotal() {
+    let totalPrice = 0;
+    
+    // Loop through the price elements and sum their values
+    priceElements.forEach((element) => {
+        totalPrice += parseFloat(element.textContent);
+    });
+    
+    // Get the grand total element
+    const grandTotalElement = document.querySelector('[data-ns-test="grandTotal"]');
+    
+    // Update the grand total element with the calculated total price
+    grandTotalElement.textContent = totalPrice.toFixed(2);
+}
 
-let newRow = document.createElement("tr");
-let newCell = document.createElement("td");
-
-newCell.textContent= `Total: $${totalPrice.toFixed(2)}`;
-
-newRow.appendChild(newCell);
-let table = document.getElementsByTagName("table")[0];
-table.appendChild(newRow);
-
-
+// Call the updateTotal function to calculate and display the initial total
+updateTotal();
