00d47866c4dc7a5602dacc33d7a4d89c4b8fb2eb
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-08 17:13:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //your code here
-let priceElement = document.getElementById("data-ns-prices");
+let priceElement = document.querySelectorAll("data-ns-prices");
 let totalPrice = 0;
 
-priceElement.forEach((priceElement)=>
+priceElement.forEach((priceElement)=>{
 	let price = parseInt(priceElement.textContent);
 			totalPrice += price;		 
-	);
+});
 
 let newRow = document.createElement("tr");
 let newCell = document.createElement("td");
@@ -13,7 +13,7 @@ let newCell = document.createElement("td");
 newCell.textContent= `Total: $${totalPrice.toFixed(2)}`;
 
 newRow.appendChild(newCell);
-let table = document.getElementByTagname("table");
+let table = document.getElementsByTagname("table");
 table.appendChild(newRow);
 
 caluculateTotalPrice();
